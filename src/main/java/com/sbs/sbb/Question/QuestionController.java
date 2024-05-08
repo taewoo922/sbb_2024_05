103c6498ab15984c20e1d4505e55afaccfbd708f
taewoo922 <kimtaewoo922@gmail.com>
2024-05-08 17:46:41 +0900

    답변 등록 폼

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,5 +29,11 @@ public class QuestionController {
         model.addAttribute("question", q);
 
         return "question_detail";
+    }
+
+    @PostMapping("/createTemp")
+    @ResponseBody
+    public String temp() {
+        return "전송완료";
     }
 }
