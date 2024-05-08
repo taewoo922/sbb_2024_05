9c42dd27b555e03e51fa562242413d40b24634f5
taewoo922 <kimtaewoo922@gmail.com>
2024-05-08 18:22:42 +0900

    답변 컨트롤러 추가

---
+++ src/main/java/com/sbs/sbb/Answer/AnswerController.java
@@ -0,0 +1,27 @@
+package com.sbs.sbb.Answer;
+
+import com.sbs.sbb.Question.Question;
+import com.sbs.sbb.Question.QuestionRepository;
+import com.sbs.sbb.Question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+        //답변 부모 질문객체를 받아온다.
+        Question q = this.questionService.getQuestion(id);
+
+        return "redirect:/question/detail/%d".formatted(id);
+    }
+}
--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -30,10 +30,4 @@ public class QuestionController {
 
         return "question_detail";
     }
-
-    @PostMapping("/createTemp")
-    @ResponseBody
-    public String temp() {
-        return "전송완료";
-    }
 }
