c85a979a8bc567525d15e464e89d5468d917a84f
taewoo922 <kimtaewoo922@gmail.com>
2024-05-08 17:18:11 +0900

    exception추가 detail수정

--- src/main/java/com/sbs/sbb/Question/QuestionService.java
+++ src/main/java/com/sbs/sbb/Question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.sbs.sbb.Question;
 
+import com.sbs.sbb.DataNotException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,16 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = this.questionRepository.findById(id);
+
+        // oq.isPresent() == false
+//        !oq.isPresent()
+        // oq.isEmpty()
+        if ( oq.isEmpty() ) throw new DataNotException("question not found");
+
+        return oq.get();
     }
 }
