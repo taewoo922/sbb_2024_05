1d34981e2c547588303d34d946249eb8175b69f0
taewoo922 <kimtaewoo922@gmail.com>
2024-05-10 14:35:05 +0900

    validation 추가

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -1,9 +1,11 @@
 package com.sbs.sbb.Question;
 
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
+//@Validated 컨트롤러에서는 이 부분 생략가능
 public class QuestionController {
     private final QuestionService questionService;
 
@@ -37,11 +40,9 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String questionCreate(QuestionForm questionform) {
-        String subject = questionform.getSubject();
-        String content = questionform.getSubject();
-
-        Question q = this.questionService.create(subject, content);
+    // QuestionForm 값을 바인딩 할 떄 유효성 체크를 해라!
+    public String questionCreate(@Valid QuestionForm questionform) {
+        Question q = this.questionService.create(questionform.getSubject(), questionform.getSubject());
 
         return "redirect:/question/list";
     }
