1419d48595b06faf442993a78fb3beb9f71ce17c
taewoo922 <kimtaewoo922@gmail.com>
2024-05-13 16:40:38 +0900

    회원 엔티티, 회원 리포지터리, 회원 서비스 생성

---
+++ src/main/java/com/sbs/sbb/user/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sbs.sbb.user;
+
+import com.sbs.sbb.Answer.Answer;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<SiteUser, Integer> {
+    @Transactional
+    @Modifying
+    @Query(value="ALTER TABLE site_user AUTO_INCREMENT = 1", nativeQuery = true)
+    void createAutoIncreament();
+}
--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -5,6 +5,8 @@ import com.sbs.sbb.Answer.AnswerRepository;
 import com.sbs.sbb.Question.Question;
 import com.sbs.sbb.Question.QuestionRepository;
 import com.sbs.sbb.Question.QuestionService;
+import com.sbs.sbb.user.UserRepository;
+import com.sbs.sbb.user.UserService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,9 +28,13 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionService questionService;
 	@Autowired
+	private UserService userService;
+	@Autowired
 	private QuestionRepository questionRepository;
 	@Autowired
 	private AnswerRepository answerRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 	@BeforeEach
 		// 아래 메서드는 각 테스트케이스가 실행되기 전에 실행된다.
@@ -40,6 +46,13 @@ class SbbApplicationTests {
 		// 모든 데이터 삭제
 		questionRepository.deleteAll();
 		questionRepository.createAutoIncreament();
+
+		// 모든 데이터 삭제(흔적 삭제 -> 다음번 INSERT를 할 떄 id가 1번으로 설정되도록)
+		userRepository.createAutoIncreament();
+
+		//회원 2명 생성
+		userService.create("user1", "user1@test.com", "1234");
+		userService.create("user2", "user2@test.com", "1234");
 
 		// 질문 1개 생성
 		Question q1 = new Question();
