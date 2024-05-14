6895e6f9b303d69116509deb3f8760b8c6fc1933
taewoo922 <kimtaewoo922@gmail.com>
2024-05-14 16:41:20 +0900

    질문 수정

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -4,15 +4,16 @@ import com.sbs.sbb.Answer.AnswerForm;
 import com.sbs.sbb.user.SiteUser;
 import com.sbs.sbb.user.UserService;
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -25,16 +26,16 @@ public class QuestionController {
     private final QuestionService questionService;
     private final UserService userService;
 
-
     @GetMapping("/list")
     public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
         Page<Question> paging = this.questionService.getList(page);
         model.addAttribute("paging", paging);
+
         return "question_list";
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerFrom) {
         Question q = this.questionService.getQuestion(id);
 
         model.addAttribute("question", q);
@@ -44,27 +45,66 @@ public class QuestionController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
-    // QuestionForm 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
-    // QuestionForm questionForm 써주는 이유 : question_form.html 에서 questionForm 변수가 없으면 실행이 안되기 때문에
+    // QuestionFrom 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
+    // QuestionFrom questionForm 써주는 이유 : question_form.html에서  questionForm 변수가 없으면 실행이 안되기 때문에
     // 빈 객체라도 만든다.
-//    public String questionCreate(Model model) {
-    public String questionCreate(QuestionForm questionForm) {
-//        model.addAttribute("questionForm", new QuestionForm());
+    // public String create(Model modle) {
+    public String create(QuestionForm questionFrom) {
+//        model.addAttribute("questionFrom", new QuestionForm());
+
         return "question_form";
     }
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
-    // QuestionForm 값을 바인딩 할 떄 유효성 체크를 해라!
-    public String questionCreate(@Valid QuestionForm questionform, BindingResult bindingResult, Principal principal) {
-        if (bindingResult.hasErrors() ) {
+    // QuestionForm 값을 바인딩 할 때 유효성 체크를 해라!
+    // QuestionFrom 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
+    public String questionCreate(
+            @Valid QuestionForm questionForm,
+            BindingResult bindingResult,
+            Principal principal) {
+
+        if ( bindingResult.hasErrors() ) {
             // question_form.html 실행
-            // 다시 작성하라는 의마로 응답에 폼을 싫어서 보냄
+            // 다시 작성하라는 의미로 응답에 폼을 싫어서 보냄
             return "question_form";
         }
+
         SiteUser siteUser = this.userService.getUser(principal.getName());
-        Question q = this.questionService.create(questionform.getSubject(), questionform.getSubject(), siteUser);
+        Question q = this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
 
         return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+                                 Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        Question question = this.questionService.getQuestion(id);
+
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
     }
 }
